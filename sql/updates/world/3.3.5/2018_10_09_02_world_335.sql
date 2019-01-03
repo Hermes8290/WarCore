3186bb2c1e19c2b0c16dc47b7019ba3a861ed686
DevRainDash <raindashwarlog@gmail.com>
2019-01-03 16:04:27 +0100

    Add TrinityCoreWotlk fork

---
+++ sql/updates/world/3.3.5/2018_10_09_02_world_335.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `quest_offer_reward_locale`;
+CREATE TABLE `quest_offer_reward_locale` (
+  `ID` int(10) unsigned NOT NULL DEFAULT '0',
+  `locale` varchar(4) NOT NULL,
+  `RewardText` text,
+  `VerifiedBuild` smallint(6) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`locale`)
+);
+
+DROP TABLE IF EXISTS `quest_request_items_locale`;
+CREATE TABLE `quest_request_items_locale` (
+  `ID` int(10) unsigned NOT NULL DEFAULT '0',
+  `locale` varchar(4) NOT NULL,
+  `CompletionText` text,
+  `VerifiedBuild` smallint(6) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`locale`)
+);
